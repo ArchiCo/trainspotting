0cad615d8a02784e35e5f26e9383c2089df40653
Ilja Pavlov <iljap@student.chalmers.se>
2018-01-22 14:42:17 +0100

    Forming threading structure and the distinction between Train objects.

--- src/trainspotting/Lab1.java
+++ src/trainspotting/Lab1.java
@@ -3,15 +3,52 @@ import TSim.*;
 
 public class Lab1 {
 
-  public Lab1(Integer speed1, Integer speed2) {
+  public Lab1(Integer speed1, Integer speed2){
     TSimInterface tsi = TSimInterface.getInstance();
 
     try {
-      tsi.setSpeed(1,speed1);
-    }
+    	Train train1 = new Train(1, tsi);
+    	Train train2 = new Train(2, tsi);
+    	train1.setSpeed(speed1);
+    	train2.setSpeed(speed2);
+    	train1.start();
+    	train2.start();
+   }
     catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
       System.exit(1);
     }
+  }
+  
+  class Train extends Thread {
+	  private int id;
+	  private int speed;
+	  private TSimInterface tsi;
+	  
+	  public Train(int id, TSimInterface tsi) {
+		  this.id = id;
+		  this.tsi = tsi.getInstance();
+	  }
+	  
+	  public void setSpeed(int speed) throws CommandException {
+		  this.speed = speed;
+		  tsi.setSpeed(id, speed);
+	  }
+	  
+	  public int getStatus() {
+		  if (speed > 0) { 
+			  return  1; } // If train's moving forward
+		  else if (speed < 0) { 
+			  return -1; } // If train's moving backwards
+		  else {
+			  return 0;    // If train's stopped
+		  }
+	  }
+	  
+	  public void run() {
+		  while (true) {
+			  
+		  }
+	  }  
   }
 }
