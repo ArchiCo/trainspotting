0c677ce7aacad61158d5a4deecf8daa3465f0c4e
Ilja Pavlov <iljap@student.chalmers.se>
2018-01-24 14:10:38 +0100

    Added prototype skeleton for sensors.

--- src/trainspotting/Lab1.java
+++ src/trainspotting/Lab1.java
@@ -46,7 +46,7 @@ public class Lab1 {
 		  tsi.setSpeed(trainId, speed);
 	  }
 	  
-	  public int getStatus() {
+	  private int getStatus() {
 		  if (speed > 0) { 
 			  return  1; } // If train's moving forward
 		  else if (speed < 0) { 
@@ -60,7 +60,44 @@ public class Lab1 {
 		  while (true) {
 			  try {
 				SensorEvent sensor = tsi.getSensor(trainId);
+				switch(Integer.toString(sensor.getXpos()) +
+				       Integer.toString(sensor.getYpos())) {
+				// Train stops, waits, and reverses movement. Falls through for all conditions.
+				case "16,3":
+				case "16,5":
+				case "16,11":
+				case "16,13":
+					break;
 				
+				// Crossroad cases
+				case "6,7":
+					break;
+				case "10,7":
+					break;
+				case "8,5":
+					break;
+				case "10,8":
+					break;
+				
+				case "3,14": // SSL & OL2
+					break;
+				case "5,11": // SSL & OL2
+					break;
+				case "5,9":  // OL2 & Middle
+					break;
+				case "5,10": // OL2 & Middle
+					break;
+				
+				case "15,7": // NSL & OL1
+					break;
+				case "15,8": // NSL & OL1
+					break;
+				case "14,9": // OL1 & Middle
+					break;
+				case "14,10":// OL1 & Middle
+					break;
+				
+				}
 							
 			} catch (CommandException | InterruptedException e) {
 				// TODO Auto-generated catch block
